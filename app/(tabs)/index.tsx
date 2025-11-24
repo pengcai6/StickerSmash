c0d60ab51592864400c5709fbf1db28a472bfc27
cai <3538616223@qq.com>
2025-11-24 13:48:43 +0800

    重构主页，添加图片查看器和按钮组件；更新404页面和样式

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
-import { View,Text,StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+import { StyleSheet, View } from "react-native";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Index</Text>
-      <Link href={"/about"} style={styles.button}>
-        {" "}
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage}></ImageViewer>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="请选择图片" theme="primary" />
+        <Button label="使用该图片" />
+      </View>
     </View>
   );
 }
@@ -19,14 +24,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
