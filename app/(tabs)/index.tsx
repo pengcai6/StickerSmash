ea510bb8b15cce2959678471aacffd3922c9082e
cai <3538616223@qq.com>
2025-11-26 13:20:09 +0800

    重构EmojiSticker组件，添加手势支持以实现缩放和拖动功能

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,5 @@
 import * as ImagePicker from "expo-image-picker";
 import { ImageSourcePropType, StyleSheet, View } from "react-native";
-
-import { useState } from "react";
-
 import Button from "@/components/Button";
 import CircleButton from "@/components/CircleButton";
 import EmojiList from "@/components/EmojiList";
@@ -10,6 +7,8 @@ import EmojiPicker from "@/components/EmojiPicker";
 import EmojiSticker from "@/components/EmojiSticker";
 import IconButton from "@/components/IconButton";
 import ImageViewer from "@/components/ImageViewer";
+import { useState } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
@@ -52,52 +51,53 @@ export default function Index() {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer
-          imgSource={PlaceholderImage}
-          selectedImage={selectedImage}
-        />
-        {pickedEmoji && (
-          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
-        )}
-      </View>
-      {showAppOptions ? (
-        <View style={styles.optionsContainer}>
-          <View style={styles.optionsRow}>
-            <IconButton
-              icon="refresh"
-              label="重置"
-              onPress={onReset}
-            ></IconButton>
-            <CircleButton onPress={onAddSticker}></CircleButton>
-            <IconButton
-              icon="save"
-              label="保存"
-              onPress={onSaveImageAsync}
-            ></IconButton>
+    <GestureHandlerRootView style={styles.container}>
+
+        <View style={styles.imageContainer}>
+          <ImageViewer
+            imgSource={PlaceholderImage}
+            selectedImage={selectedImage}
+          />
+          {pickedEmoji && (
+            <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+          )}
+        </View>
+        {showAppOptions ? (
+          <View style={styles.optionsContainer}>
+            <View style={styles.optionsRow}>
+              <IconButton
+                icon="refresh"
+                label="重置"
+                onPress={onReset}
+              ></IconButton>
+              <CircleButton onPress={onAddSticker}></CircleButton>
+              <IconButton
+                icon="save"
+                label="保存"
+                onPress={onSaveImageAsync}
+              ></IconButton>
+            </View>
           </View>
-        </View>
-      ) : (
-        <View style={styles.footerContainer}>
-          <Button
-            theme="primary"
-            label="Choose a photo"
-            onPress={pickImageAsync}
-          />
-          <Button
-            label="Use this photo"
-            onPress={() => setShowAppOptions(true)}
-          />
-        </View>
-      )}
-      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
-        <EmojiList
-          onCloseModal={onModalClose}
-          onSelect={setPickedEmoji}
-        ></EmojiList>
-      </EmojiPicker>
-    </View>
+        ) : (
+          <View style={styles.footerContainer}>
+            <Button
+              theme="primary"
+              label="Choose a photo"
+              onPress={pickImageAsync}
+            />
+            <Button
+              label="Use this photo"
+              onPress={() => setShowAppOptions(true)}
+            />
+          </View>
+        )}
+        <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+          <EmojiList
+            onCloseModal={onModalClose}
+            onSelect={setPickedEmoji}
+          ></EmojiList>
+        </EmojiPicker>
+    </GestureHandlerRootView>
   );
 }
 
