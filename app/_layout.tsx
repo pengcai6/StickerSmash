fb0064dae7a7c82f9908ae58a01ba1d9d42fc5b6
cai <3538616223@qq.com>
2025-11-26 20:01:03 +0800

    重构RootLayout组件，调整StatusBar位置以改善布局

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
-
+import { StatusBar } from "expo-status-bar";
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
   );
 }
